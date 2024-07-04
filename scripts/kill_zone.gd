de1308ca27e779de45f7cf5374e19f85930fd51d
tomasferencik <tomife2@gmail.com>
2024-07-04 22:48:44 +0200

    added hurt sound and decreased camera bottom limit

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var hurt_sound = $HurtSound
 
 func _on_body_entered(body):
 	print("You died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	hurt_sound.play()
 	timer.start()
 
 
